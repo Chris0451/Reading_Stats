5264407f150af956b6712a41fcd3175c68329e3b
Chris_DC <dcchris56@gmail.com>
2025-09-25 20:15:27 +0200

    Update Kotlin tooling versions

---
+++ app/src/main/java/com/project/reading_stats/ReadingStatsApp.kt
@@ -0,0 +1,7 @@
+package com.project.reading_stats
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ReadingStatsApp : Application()
--- app/src/main/java/com/project/reading_stats/core/ui/theme/Theme.kt
+++ app/src/main/java/com/project/reading_stats/core/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.project.reading_stats.core.theme
+package com.project.reading_stats.core.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Reading_StatsTheme(
+fun ReadingStatsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
